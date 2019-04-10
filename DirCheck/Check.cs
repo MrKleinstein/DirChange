6662d026349ae41a1fabb70d93ee6a7bf6fea1e8
Tim <t.kleinoth@gmail.com>
2019-04-10 21:23:28 +0200

    First Command

---
+++ DirCheck/Check.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirCheck
+{
+    class Check
+    {
+        private FileSystemWatcher watcher;
+        private string path = @"C:\Users\tklei\OneDrive\Desktop";
+
+        public Check()
+        {
+            watcher = new FileSystemWatcher(path);
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName;
+
+        }
+
+        public void Start()
+        {
+            watcher.Created+=new FileSystemEventHandler(OnCreated);
+            watcher.EnableRaisingEvents = true;
+            watcher.IncludeSubdirectories = true;
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            string cha = "Es wurde die Datei "+e.Name+" hinzugefuegt!";
+            File.WriteAllText(@"C:\Users\tklei\OneDrive\Desktop\changed.txt",cha );
+        }
+
+
+        public void Stop()
+        {
+            watcher.EnableRaisingEvents = false;
+        }
+    }
+}
---
+++ DirCheck/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace DirCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<Check>(s =>
+                {
+                    s.ConstructUsing(check => new Check());
+                    s.WhenStarted(check => check.Start());
+                    s.WhenStopped(check => check.Stop());
+                });
+
+                x.RunAsLocalSystem();
+                x.SetServiceName("DirCheck");
+                x.SetDisplayName("Directory Check");
+                x.SetDescription("Check directory and move files if amount >100");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
